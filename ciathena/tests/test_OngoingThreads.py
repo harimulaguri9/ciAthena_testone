2d7d15bc19733252a9911010fd852c15448ab718
Hari Kumar  Mulaguri <hari.mulaguri@customerinsights.ai>
2026-02-04 09:54:24 +0530

    added users auth scripts

--- ciathena/tests/test_OngoingThreads.py
+++ ciathena/tests/test_OngoingThreads.py
@@ -2,7 +2,7 @@
 import pytest
 @pytest.mark.asyncio
 @pytest.mark.order(2)
-@pytest.mark.wip
+# @pytest.mark.wip
 @pytest.mark.asyncio
 async def test_ask_question(setup):
     ongoingthreadsPage = setup["ongoingthreadsPage"]
